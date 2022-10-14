2ccb43adc82e424245c24dbf29049c7bc0896083
Albin_Dalipi_techinnov <albin.dalipi@techinnov.ca>
2022-10-14 13:50:55 +0200

    Added routers and adds tyling to navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import Directory from "./component/directory/directory.component";
+import Home from "./routes/home/home.component"
+import {Routes , Route} from 'react-router-dom'
+
+import Navigation from "./routes/navigation/navigation.component"
+
+
+const Shop = () => {
+
+  return(
+    <div>
+      SHOP
+    </div>
+  )
+
+}
 
 const App = () => {
 
-  return (
-    <Directory/>
+  return(
+    <Routes>
+      <Route path="/" element = {<Navigation/>}>
+        <Route index element = {<Home/>}/>
+        <Route path = "/shop" element = {<Shop/>}/>
+      </Route>  
+    </Routes>
+    
   )
 
 }
