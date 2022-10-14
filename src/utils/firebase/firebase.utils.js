ee6d65d43890c57a99fac4fdc710a36dcfb16d2a
Albin_Dalipi_techinnov <albin.dalipi@techinnov.ca>
2022-10-14 18:44:58 +0200

    Added sign in functionality as well as storing users in database through firestore

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,59 @@
+import {initializeApp} from "firebase/app"
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider
+}from "firebase/auth"
+import {getFirestore, doc, getDoc,setDoc} from "firebase/firestore"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDv2PPkQK4c4MG-sb77gf5uJvq4a8K4QLA",
+    authDomain: "stylex-clothing-db.firebaseapp.com",
+    projectId: "stylex-clothing-db",
+    storageBucket: "stylex-clothing-db.appspot.com",
+    messagingSenderId: "439899317085",
+    appId: "1:439899317085:web:1b10feb600baa1db86feb4"
+  };
+  
+// Initialize Firebase
+const firebaseapp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt : "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth,provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+
+    const userDocRef = doc(db, "users", userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    //if user does not exist
+    if(!userSnapshot.exists()){
+
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try{    
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        }catch(error){
+            console.log("There was an error creating user", error.message);
+        }
+    }
+
+    return userDocRef;
+
+}
